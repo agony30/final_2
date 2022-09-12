91a2517f637664183a3951f10b44fb3d89df5ac9
Admin <agony30@yandex.ru>
2022-09-12 17:15:11 +0500

    test_product_page.py - настроил параметризацию теста, для проверки всех скидочных предложений.

--- example.py
+++ example.py
@@ -1,4 +1,29 @@
-import math
+import pytest
 
-x = 218
-print(str(math.log(abs((12 * math.sin(float(x)))))))
+
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
+a = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+
+b = [f"{product_base_link}/?promo=offer{no}" if no != 7
+        else pytest.param(f"{product_base_link}/?promo=offer{no}", marks=pytest.mark.xfail) for no in range(10)]
+
+
+for i, link in enumerate(a):
+
+    if i > 6:
+        print(link)
+print()
+
+for i, link in enumerate(b):
+    if i > 6:
+        print(link)
--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,18 @@
+import pytest
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
 import time
 
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+urls = [f"{product_base_link}/?promo=offer{no}" if no != 7
+        else pytest.param(f"{product_base_link}/?promo=offer{no}", marks=pytest.mark.xfail) for no in range(10)]
+
+
+
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"{link}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
@@ -22,4 +30,3 @@ def test_guest_can_add_product_to_basket(browser):
     basket_total_sum = float(basket_text[basket_text.find('£') + 1:basket_text.find('\n')])  # сумма в корзине
     assert float(page.browser.find_element(*ProductPageLocators.PRICE).text[1:]) == basket_total_sum, 'Цена не равна'
     print('Сумма корзины:', basket_total_sum)
-
